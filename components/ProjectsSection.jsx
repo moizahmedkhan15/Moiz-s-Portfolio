11979df627e54d5eea7971caf218219b61977005
moizahmedkhan15 <moizahmedkhan15@gmail.com>
2025-07-23 16:16:18 +0500

    arranged the data for more perfection

--- components/ProjectsSection.jsx
+++ components/ProjectsSection.jsx
@@ -1,36 +1,5 @@
 import ProjectCard from "./ProjectCard";
-
-const projects = [
-  {
-    title: "Django Ecommerce WebApp",
-    description:
-      "Full stack functioning Django and Python based ecommerce website with add to cart, login/signup, admin dashboard and database logic.",
-    imgSrc: "/projects/Capture.png",
-    codeLink: "https://github.com/moizahmedkhan15/eshop-ecommerce",
-    tech: "Django, Python, HTML, CSS, Bootstrap."
-  },
-  {
-    title: "React Todo WebApp",
-    description: "A clean and interactive ToDo web application built with ReactJS, allowing users to add and delete tasks.",
-    imgSrc: "/projects/reactCapture.png",
-    codeLink: "https://github.com/moizahmedkhan15/React-Todo",
-    tech: "React.js, CSS Modules."
-  },
-  {
-    title: "JavaScript Time Teller",
-    description: "A clean and interactive HTML And JavaScript based Time Teller Website which shows current time of different zones.",
-    imgSrc: "/projects/clockCapture.png",
-    codeLink: "https://github.com/moizahmedkhan15/CurrentTime",
-    tech: "Html, Javascript."
-  },
-    {
-    title: "Rock, Paper, Scissor",
-    description: "A Mini Game Of Rock, Paper, Scissor which is based on Python Language.",
-    imgSrc: "/projects/rpsCapture.png",
-    codeLink: "https://github.com/moizahmedkhan15/rock-paper-scissor",
-    tech: "Python."
-  }
-];
+import projects from "./projectsData";
 
 export default function ProjectsSection() {
   return (
